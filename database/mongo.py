1d071d5614258394eac3b59bc831e29888b502f1
PP <ppaulo030601@gmail.com>
2025-04-24 19:44:15 -0300

    Refactor MongoDB connection handling and improve error logging

--- database/mongo.py
+++ database/mongo.py
@@ -1,11 +1,23 @@
-import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from loguru import logger
 
+MONGO_USER = 'user_pi'
+MONGO_PASSWORD = 'XpIAmprxBkT59jet'
+uri = f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@cluster0.b77cm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+local_uri = "mongodb://localhost:27017/"
+try: 
+    logger.info("Connecting to MongoDB Atlas...")
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    client.admin.command('ping')
+    logger.info("MongoDB Atlas connection successful")
+except Exception as e:
+    logger.error(f"Error connecting to MongoDB: {e}")
+    logger.warning("Using local MongoDB instance")
+    client = MongoClient(local_uri)
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client['projeto_integrador']
 collection_leituras = db['leituras']
-
-
 def aggregate(collection, pipeline):
     result = list(collection.aggregate(pipeline))
     for doc in result:
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from routes import leituras, coordenadas
 
 app = FastAPI()
@@ -13,7 +12,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.get("/")
 async def home():
     return {"message": "Hello World"}
@@ -21,6 +19,7 @@ async def home():
 app.include_router(leituras.router, prefix="/leituras", tags=["leituras"])
 app.include_router(coordenadas.router, prefix="/coordenadas", tags=["coordenadas"])
 
+#uvicorn main:app --reload --host 0.0.0.0 --port 81
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=81, reload=True)
